97e21c9c8e6ce16d6d5d09f7198d3be2f62e34e5
zzh <v1otusc@yeah.net>
2023-05-23 11:25:47 +0800

    Update Nocopyable.hpp

--- Nocopyable.hpp
+++ Nocopyable.hpp
@@ -1,3 +1,6 @@
+#ifndef __NOCOPYABLE_H__
+#define __NOCOPYABLE_H__
+
 class Nocopyable {
  public:
   Nocopyable(){};
@@ -6,3 +9,5 @@ class Nocopyable {
   Nocopyable(const Nocopyable &) = delete;
   Nocopyable &operator=(const Nocopyable &) = delete;
 };
+
+#endif // __NOCOPYABLE_H__
